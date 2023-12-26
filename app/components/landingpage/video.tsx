3550383df28c28bad95d69d8e48b34730998bfab
Quynh Dang Thi Diem <dangdiemquynh47@gmail.com>
2023-12-26 17:51:26 +0700

    update

--- app/components/landingpage/video.tsx
+++ app/components/landingpage/video.tsx
@@ -1,8 +1,7 @@
 const Video = () => {
   return (
-    <div className="w-full md:p-20 p-6">
-     <iframe className="h-[685px] w-full" src="https://www.youtube.com/embed/zepHPnUDROE?si=XPZdJvCZY6paGmCg" allowFullScreen>
-     </iframe>
+    <div className="w-full md:px-40 md:py-20 p-6">
+     <iframe width="100%" height="600px" src="https://www.youtube.com/embed/CY6QP4ofwx4?si=A42J2Xtps3IMt2_u" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   );
 };
