c6af261294619a1afba1f31dc14640adaceba251
Quynh Dang Thi Diem <dangdiemquynh47@gmail.com>
2024-03-13 17:14:44 +0700

    update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,21 @@ export const metadata: Metadata = {
   title: "Mala Yoga",
   description:
     "If you`re here and read a little about me, I believe the universe is connecting us Surely you are also a Yogi/Yogini or the universe has sent a signal for you to touch your finger to touch Yoga Hope my sharing will help you in this journey Namaste",
-    openGraph: {
-      title: "Mala Yoga",
-      description: "",
-      url: "https://malayoga.vn",
-      type: "website",
-      locale: "vi_VN",
-      siteName: "MALA YOGA",
-      images: [
-        {
-          url: "/images/malayoga/IMG_0802.jpg",
-          alt: "Mala Yoga",
-        },
-      ],
-    },
-  
+  metadataBase: new URL("https://malayoga.vn"),
+  openGraph: {
+    title: "Mala Yoga",
+    description: "",
+    url: "https://malayoga.vn",
+    type: "website",
+    locale: "vi_VN",
+    siteName: "MALA YOGA",
+    images: [
+      {
+        url: "/images/malayoga/IMG_0802.jpg",
+        alt: "Mala Yoga",
+      },
+    ],
+  },
 };
 export default function Home() {
   return (
@@ -30,4 +30,4 @@ export default function Home() {
 }
 
 // export const revalidate = 60;
-// 
+//
